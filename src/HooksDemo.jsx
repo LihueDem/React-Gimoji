d37d5c42b249ec5f40cb01e13ba02111452146ab
Lihue <lihue.demelchiorre@gmail.com>
2024-02-12 00:03:40 -0300

    Proyecto terminado

---
+++ src/HooksDemo.jsx
@@ -0,0 +1,30 @@
+import React, { useMemo, useState } from "react";
+import { useCallback } from "react";
+
+export const HooksDemo = () => {
+  const [cont, setCont] = useState(0);
+
+  const onClickSumar = () => {
+    setCont((prev) => prev + 1);
+  };
+
+  const dobleContador = () => {
+    return cont * 2;
+  };
+
+  //memoriza la funcion
+  const callback = useCallback(() => dobleContador(), []);
+  //memoriza el resultado
+  const memo = useMemo(() => dobleContador(), []);
+
+  console.log(callback, "CALLBACK");
+  console.log(callback());
+  console.log(memo, "MEMO");
+
+  return (
+    <div className=" d-flex row justify-content-center mt-5">
+      <div>NUM: {cont}</div>
+      <button onClick={onClickSumar}> AGREGAR </button>
+    </div>
+  );
+};
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useId, useRef, useEffect } from "react";
 
 export default function Search({ onClickSearch }) {
-  const [dataText, setDataText] = useState("");
-  const onChangeSearch = (event) => {
-    setDataText(event.target.value);
+  // const [dataText, setDataText] = useState("");
+  const id = useId();
+  const refInputSearch = useRef();
+  const refBtnSearch = useRef();
+
+  useEffect(() => {
+    refInputSearch.current.focus();
+    refBtnSearch.current.className = "btn btn-primary disabled";
+  }, []);
+
+  const onChangeSearch = () => {
+    if (refInputSearch.current.value.length > 4) {
+      refBtnSearch.current.className = "btn btn-success";
+    }
   };
 
   return (
     <div className="row">
       <div className="col-md-10">
         <input
+          id={id}
+          ref={refInputSearch}
           type="search"
           className=" form-control form-control-dark text-dark"
           placeholder="Search..."
@@ -19,9 +32,10 @@ export default function Search({ onClickSearch }) {
       </div>
       <div className="col-md-2">
         <button
+          ref={refBtnSearch}
           type="button"
-          className="btn btn-primary "
-          onClick={() => onClickSearch(dataText)}
+          className="btn btn-primary"
+          onClick={() => onClickSearch(refInputSearch.current.value)}
         >
           BUSCAR
         </button>
